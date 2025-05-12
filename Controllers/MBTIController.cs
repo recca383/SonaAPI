dfc5204bbd8e48cf5c80d025400474332556f1b3
recca383 <sirpatrick121402@gmail.com>
2025-05-13 00:37:31 +0800

    Removed template items, added controller to test spotify API response

---
+++ Controllers/MBTIController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SonaAPI.Repository;
+
+namespace SonaAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MBTIController : ControllerBase
+    {
+        private readonly SpotifyRepository spotifyRepository;
+
+        public MBTIController(SpotifyRepository spotifyRepository)
+        {
+            this.spotifyRepository = spotifyRepository;
+        }
+
+        [HttpGet("{playlistId}")]
+        public async Task<IActionResult> GetPlaylist(string playlistId)
+        {
+            var accessToken = await spotifyRepository.GetAccessTokenAsync();
+
+            var result = await spotifyRepository.GetPlaylist(playlistId, accessToken);
+
+            if(result != null)
+                return Ok(result);
+
+            return NotFound();
+        }
+    }
+}
