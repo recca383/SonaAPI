5c37db67e52d021f0d809b55ab326302a033b10e
recca383 <sirpatrick121402@gmail.com>
2025-05-13 21:00:01 +0800

    Added ML module to predict playlist based on float values mean

---
+++ ML/MBTIColumns.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.ML.Data;
+
+namespace SonaAPI.ML
+{
+    public class MBTIColumns
+    {
+        // Playlist ID
+        [LoadColumn(2)]
+        public required string Label { get; set; }
+        // MBTI 4 letter Code 
+        [LoadColumn(0)]
+        public required string MBTI { get; set; }
+
+        [LoadColumn(1)]
+        public required string function_pair { get; set; }
+
+        [LoadColumn(5)]
+        public float danceability_mean { get; set; }
+
+        [LoadColumn(17)]
+        public float liveness_mean { get; set; }
+
+        [LoadColumn(19)]
+        public float valence_mean { get; set; }
+
+        [LoadColumn(7)]
+        public float energy_mean { get; set; }
+
+        [LoadColumn(23)]
+        public float instrumentalness_mean { get; set; }
+
+        [LoadColumn(9)]
+        public float loudness_mean { get; set; }
+
+        [LoadColumn(21)]
+        public float tempo_mean { get; set; }
+
+        
+    }
+}
---
+++ ML/MBTIPrediction.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.ML.Data;
+
+namespace SonaAPI.ML
+{
+    public class MBTIPrediction
+    {
+        [ColumnName("playlist_id")]
+        public string? playlist_id { get; set; }
+    }
+}
