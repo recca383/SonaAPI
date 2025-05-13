9334d3fbbfb7751c84f8ece0143fc59830e3257b
recca383 <sirpatrick121402@gmail.com>
2025-05-14 01:07:25 +0800

    Added segregation between dataset using MBTI codes to lessen training time

--- Repository/MBTIRepository.cs
+++ Repository/MBTIRepository.cs
@@ -4,6 +4,7 @@ using SonaAPI.ML;
 using SonaAPI.Models;
 using SonaAPI.Repository.Contracts;
 using System.Data;
+using System.Linq.Expressions;
 
 namespace SonaAPI.Repository
 {
@@ -12,30 +13,32 @@ namespace SonaAPI.Repository
         public Task<string> GetPlaylistID(MBTI mbti)
         {
             var context = new MLContext();
+            string filename = mbti.Mbti.Trim().ToUpper() +"_df.csv";
 
-            var data = context.Data.LoadFromEnumerable(new List<MBTI>());
-
-
-            switch (mbti.Mbti)
-            {
-                case:
-            }
             var data = context.Data.LoadFromTextFile<MBTIColumns>
-                ("Dataset/combined_mbti_df.csv"
+                ($"Dataset/{filename}"
                 , separatorChar: ','
                 , hasHeader: true);
 
+            string[] columns = {"Label"
+                                ,"danceability_mean"
+                                , "liveness_mean"
+                                , "valence_mean"
+                                , "energy_mean"
+                                , "instrumentalness_mean"
+                                , "loudness_mean"
+                                , "tempo_mean"
+                                , };
+
             var pipeline =
                     context.Transforms
-                .SelectColumns("danceability_mean", "liveness_mean", "Label")
+                .SelectColumns(columns)
                 .Append(context.Transforms.Conversion.MapValueToKey("Label"))
-                .Append(context.Transforms.Concatenate( "Features"
-                                                      , "danceability_mean"
-                                                      , "liveness_mean"
+                .Append(context.Transforms.Concatenate("Features"
+                                                      , columns[1..]
                                                       ))
-                .Append(context.MulticlassClassification.Trainers.SdcaNonCalibrated())
-                .Append(context.Transforms.ProjectToPrincipalComponents("Features", rank: 10))
-                .Append(context.Transforms.Conversion.MapKeyToValue("Label"));
+                .Append(context.MulticlassClassification.Trainers.SdcaMaximumEntropy())
+                .Append(context.Transforms.Conversion.MapKeyToValue("PredictedLabel"));
 
             var model = pipeline.Fit(data);
 
@@ -43,7 +46,8 @@ namespace SonaAPI.Repository
 
             MBTIColumns topredict = new MBTIColumns()
             {
-                Label = mbti.Mbti,
+                MBTI = mbti.Mbti,
+                function_pair = mbti.Function_pair,
                 danceability_mean = mbti.Danceablitiy,
                 liveness_mean = mbti.Liveliness,
                 valence_mean = mbti.Valance,
